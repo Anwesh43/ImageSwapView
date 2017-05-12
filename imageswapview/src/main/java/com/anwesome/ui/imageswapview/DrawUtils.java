23c1cff0f88636a809e0209b3650417f780fc436
Anwesh43 <anweshthecool0@gmail.com>
2017-05-12 07:31:22 +0530

    adding DrawUtils to contain a Paint object

---
+++ imageswapview/src/main/java/com/anwesome/ui/imageswapview/DrawUtils.java
@@ -0,0 +1,10 @@
+package com.anwesome.ui.imageswapview;
+
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 12/05/17.
+ */
+public class DrawUtils {
+    public final static Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+}
