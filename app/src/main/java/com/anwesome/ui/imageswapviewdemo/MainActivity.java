df9c49b705bdd77ea88d0dbf79c25d4e3580fd79
Anwesh43 <anweshthecool0@gmail.com>
2017-05-12 08:54:21 +0530

    Using ImageSwapView in app

--- app/src/main/java/com/anwesome/ui/imageswapviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/imageswapviewdemo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.anwesome.ui.imageswapviewdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.imageswapview.ImageSwapView;
 
+public class MainActivity extends AppCompatActivity {
+    private Bitmap bitmap1,bitmap2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        if(bitmap1 == null && bitmap2 == null) {
+            bitmap1 = BitmapFactory.decodeResource(getResources(),R.drawable.back1);
+            bitmap2 = BitmapFactory.decodeResource(getResources(),R.drawable.back3);
+        }
+        ImageSwapView imageSwapView = new ImageSwapView(this);
+        imageSwapView.addMainImage(bitmap2);
+        imageSwapView.addSideCircularImage(bitmap1);
+        imageSwapView.show();
     }
 }
