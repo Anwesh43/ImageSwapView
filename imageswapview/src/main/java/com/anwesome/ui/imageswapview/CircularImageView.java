f2f8ae28a893ed2e05e33646ecdd6189bb02a871
Anwesh43 <anweshthecool0@gmail.com>
2017-05-12 07:38:12 +0530

    adding circularimage

--- imageswapview/src/main/java/com/anwesome/ui/imageswapview/CircularImageView.java
+++ imageswapview/src/main/java/com/anwesome/ui/imageswapview/CircularImageView.java
@@ -1,9 +1,7 @@
 package com.anwesome.ui.imageswapview;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.*;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -12,6 +10,8 @@ import android.view.View;
  */
 public class CircularImageView extends View {
     private Bitmap bitmap;
+    private int w,h,render = 0;
+    private CircularImage circularImage = new CircularImage();
     public CircularImageView(Context context, Bitmap bitmap) {
         super(context);
     }
@@ -19,12 +19,33 @@ public class CircularImageView extends View {
         return bitmap;
     }
     public void onDraw(Canvas canvas) {
+        if(render == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+            bitmap = Bitmap.createScaledBitmap(bitmap,w,h,true);
+        }
         canvas.drawColor(Color.argb(0,0,0,0));
+        render++;
     }
     public boolean onTouchEvent(MotionEvent event) {
         return true;
     }
     public void update(float factor) {
-
+        circularImage.update(factor);
+    }
+    private class CircularImage  {
+        private float deg = 360;
+        public void draw(Canvas canvas) {
+            canvas.drawCircle(w/2,h/2,Math.min(w,h)/2,DrawUtils.paint);
+            canvas.save();
+            Path path = new Path();
+            path.addArc(new RectF(0,0,w,h),0,deg);
+            canvas.clipPath(path);
+            canvas.drawBitmap(bitmap,0,0,DrawUtils.paint);
+            canvas.restore();
+        }
+        public void update(float factor) {
+            deg = 360*factor;
+        }
     }
 }
