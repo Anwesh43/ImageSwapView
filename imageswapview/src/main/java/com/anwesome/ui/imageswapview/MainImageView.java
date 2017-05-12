32eea15b2626ea4d59deb00df854f5dc49d6d330
Anwesh43 <anweshthecool0@gmail.com>
2017-05-12 07:31:55 +0530

    adding MainImageView

---
+++ imageswapview/src/main/java/com/anwesome/ui/imageswapview/MainImageView.java
@@ -0,0 +1,39 @@
+package com.anwesome.ui.imageswapview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 12/05/17.
+ */
+public class MainImageView extends View {
+    private float scale = 1;
+    private int render = 0;
+    private Bitmap bitmap;
+    public MainImageView(Context context,Bitmap bitmap) {
+        super(context);
+        this.bitmap = bitmap;
+    }
+    public void onDraw(Canvas canvas) {
+        if(render == 0) {
+            int w = canvas.getWidth(),h = canvas.getHeight();
+            bitmap = Bitmap.createScaledBitmap(bitmap,w,h,true);
+        }
+        canvas.drawColor(Color.BLACK);
+        canvas.save();
+        canvas.translate(canvas.getWidth()/2,canvas.getHeight()/2);
+        canvas.scale(scale,scale);
+        canvas.drawBitmap(bitmap,-bitmap.getWidth()/2,-bitmap.getHeight()/2,DrawUtils.paint);
+        canvas.restore();
+        render++;
+    }
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+    public void update(float factor) {
+        scale *= factor;
+    }
+}
